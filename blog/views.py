2bd388fd9d8ba975c61734874fb2ba80ed309dc9
Lucas Costa <lucas.carvalho7410@gmail.com>
2023-05-21 18:29:39 -0300

    Adicionado páginas

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def my_home(request):
+    return render(request, "blog/home.html")
+
+
+def my_about(request):
+    return render(request, "blog/about.html")
